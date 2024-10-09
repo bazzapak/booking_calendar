0deb6acf0868712940cdf16687267c9e62f8c8af
bazzapak <85105941+bazzapak@users.noreply.github.com>
2024-10-09 12:27:55 +0200

    Update booking_calendar_main.dart

--- lib/src/components/booking_calendar_main.dart
+++ lib/src/components/booking_calendar_main.dart
@@ -228,6 +228,7 @@ class _BookingCalendarMainState extends State<BookingCalendarMain> {
                         _focusedDay = focusedDay;
                       },
                     ),
+                    color: Color.fromRGBO(84, 81, 112, 1.0),
                   ),
                   const SizedBox(height: 8),
                   widget.bookingExplanation ??
